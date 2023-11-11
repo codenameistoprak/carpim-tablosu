cc181462c4555320ca83f695807dd197c816a7c5
Sümeyra Güçlü <guclusumeyra90@gmail.com>
2023-11-11 19:18:29 +0300

    çarpım tablosu
    Java ile çarpım tablosu

---
+++ Deneme18/src/deneme18.java
@@ -0,0 +1,21 @@
+
+public class deneme18 {
+
+	public static void main(String[] args) {
+		// iç içe for döngüsü ie çarpım tablosu
+		
+		System.out.println("\tÇARPIM TABLOSU");
+		
+		for (int i = 1; i<=10; i++) {
+			for (int j =1; j<=10; j++) {
+				
+				System.out.println(i +"x" +j + "="+ (i*j));
+			}
+			
+			System.out.println("********************");
+			
+		}
+
+	}
+
+}
